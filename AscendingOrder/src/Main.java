0142cea07474935e3a6a64453781901a3b03d04f
Sadia-Hasan123 <72305910+Sadia-Hasan123@users.noreply.github.com>
2020-10-03 19:47:03 +0600

    ascending order

---
+++ AscendingOrder/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int count,temp,i,j;
+		System.out.println("Enter how many numbers do you want to take input: ");
+		Scanner c= new Scanner(System.in);
+		count=c.nextInt();
+		
+		int a[]= new int[count];
+		System.out.println("Enter the Numbers: ");
+		for(i=0;i<count;i++) {
+			a[i]=c.nextInt();
+		}
+		for (i=0;i<count;i++) {
+			for(j=i+1;j<count;j++) {
+				if(a[i]>a[j]) {
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			 }
+		  }
+		}
+		System.out.println("Ascending order: ");
+		for(i=0;i<count;i++) {
+			System.out.print(a[i]+" ");
+		}
+	}
+
+}
